b9d234c1682634231c533f889a8faf6975c2af51
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-21 18:12:49 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   
   def actors_list 
-    self.actors.map{|actor|
+    self.actors.map{|actor| actor.full_name}
   
   
 end
