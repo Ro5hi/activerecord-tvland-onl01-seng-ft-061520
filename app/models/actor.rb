4d5ef2b8a68573fcaef5cd1845ff071f0c589559
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-21 18:50:21 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,7 +9,7 @@ class Actor < ActiveRecord::Base
   
   def list_roles 
     characters.collect do |character|
-      "#{character.name} #{character.show.name}"
+      "#{char.name} - #{char.show.name}"
       
     end 
     
